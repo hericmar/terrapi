c14c73e35f03d61a1cae8dc766de41e0605c4bc2
cube <root@phire.cz>
2022-08-13 21:52:44 +0200

    Add last fixes

--- services/install.sh
+++ services/install.sh
@@ -8,13 +8,23 @@ STATIC_ROOT="/usr/share/webapps/terrapi"
 if [ -e "broker/terrapi" ]; then
 	mkdir -p "/etc/terrapi"
 	mkdir -p "${STATIC_ROOT}"
-    
-	cp broker/terra "${BIN_PATH}"
 
-	cp "web/public" "${STATIC_ROOT}"
+	if [ ! -e "/etc/terrapi/env" ]; then
+		cp "etc/terrapi/env.example" "/etc/terrapi/env"
+		chown -R root:terra "/etc/terrapi"
+		chmod 550 "/etc/terrapi"
+		chmod 440 "/etc/terrapi/env"
+	fi
+    
+	cp "broker/terrapi" "${BIN_PATH}"
+	chown root:terra "${BIN_PATH}"
+ 
+	cp -R "web/public/." "${STATIC_ROOT}"
+	chmod -R 555 "${STATIC_ROOT}"
 
 	cp "etc/init.d/terrapi" "${SERVICE_PATH}"
-    
+	chmod 755 "${SERVICE_PATH}"
+
 	echo "Instalation completed, configure NGINX manually and then run the service."
 else
 	echo "Webapp is not build, run 'make' first."
