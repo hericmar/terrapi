f5605d98a3280f286fd411fd33ee7f46c4c0cb60
Martin Herich <martin.herich@phire.cz>
2023-03-06 21:29:25 +0100

    Remove old tests

--- terrapi/test/terrapi/test_config.cpp
+++ terrapi/test/terrapi/test_config.cpp
@@ -2,20 +2,7 @@
 
 #include "doctest/doctest.h"
 
-#include "terrapi/terrapi.h"
-
 #include "config.h"
-
-//------------------------------------------------------------------------------
-
-static constexpr const char* empty_config = R"(
-)";
-
-void test_empty_config()
-{
-    // auto* app = terra::App::create_from_string(empty_config);
-    // TEST_ASSERT(app);
-}
 
 TEST_CASE("Test example config")
 {
@@ -25,12 +12,4 @@ TEST_CASE("Test example config")
     const auto config = std::move(result.value());
     REQUIRE_EQ(config.sensors.size(), 2);
     REQUIRE_EQ(config.switches.size(), 3);
-}
-
-//------------------------------------------------------------------------------
-
-void test_config()
-{
-    test_empty_config();
-    // test_basic_config();
 }
