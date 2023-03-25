2f439ef202b7a0e11d369a529248b9488162e002
Martin Herich <martin.herich@phire.cz>
2023-03-25 11:09:23 +0100

    Update install script

--- broker/install.sh
+++ broker/install.sh
@@ -1,32 +1,37 @@
 #!/bin/sh
 
 # Alpine Linux specific!
-SERVICE_PATH="/etc/init.d/terrapi"
-BIN_PATH="/usr/bin/terrapi"
-STATIC_ROOT="/usr/share/webapps/terrapi"
+SERVICE_PATH="/etc/init.d/terrapi-broker"
+BIN_PATH="/usr/bin/terrapi-broker"
+STATIC_ROOT="/usr/share/webapps/terrapi-broker"
+TERRAPI_BROKER_USER="terra"
 
-if [ -e "broker/terrapi" ]; then
-	mkdir -p "/etc/terrapi"
-	mkdir -p "${STATIC_ROOT}"
-
-	if [ ! -e "/etc/terrapi/env" ]; then
-		cp "broker/env.example" "/etc/terrapi/env"
-		chown -R root:terra "/etc/terrapi"
-		chmod 550 "/etc/terrapi"
-		chmod 640 "/etc/terrapi/env"
-	fi
-    
-	cp "broker/terrapi" "${BIN_PATH}"
-	chown root:terra "${BIN_PATH}"
- 
-	cp -R "web/public/." "${STATIC_ROOT}"
-	chmod -R 555 "${STATIC_ROOT}"
-
-	cp "etc/init.d/terrapi" "${SERVICE_PATH}"
-	chmod 755 "${SERVICE_PATH}"
-
-	echo "Instalation completed, configure NGINX manually and then run the service."
-else
-	echo "Webapp is not build, run 'make' first."
-	exit 1
+if [ ! -f "web/dist/index.html" ]; then
+  	echo "Webapp is not build, run 'make' first."
+  	exit 1
 fi
+
+if [ ! -f "target/release/terrapi-broker" ]; then
+    echo "terrapi-broker is not build, run 'make' first."
+    exit 1
+fi
+
+if [ ! -e "/etc/terrapi-broker/config" ]; then
+    mkdir -p "/etc/terrapi-broker"
+    cp "etc/terrapi-broker/config" "/etc/terrapi-broker/config"
+    chown -R root:$TERRAPI_BROKER_USER "/etc/terrapi-broker"
+    chmod 550 "/etc/terrapi-broker"
+    chmod 640 "/etc/terrapi-broker/config"
+fi
+
+cp -R "web/dist/." "${STATIC_ROOT}"
+chown root:$TERRAPI_BROKER_USER "${STATIC_ROOT}/index.html"
+chmod -R 775 "${STATIC_ROOT}"
+
+cp "target/release/terrapi-broker" "${BIN_PATH}"
+chown root:terra "${BIN_PATH}"
+
+cp "etc/init.d/terrapi-broker" "${SERVICE_PATH}"
+chmod 755 "${SERVICE_PATH}"
+
+echo "Installation completed, configure NGINX manually and then run the service."
